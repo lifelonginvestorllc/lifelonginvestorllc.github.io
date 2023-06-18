14ba4748650a8ee937bfa6df4e21490c433eabca
wangc <wangcong26@hotmail.com>
2023-06-18 13:51:20 -0400

    Added code to process performance report

---
+++ performance/config.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+
+today = datetime.datetime.today()
+today = datetime.datetime(2023, 6, 16)
+if today.weekday() < 5:
+    NAVFilename = "IBNAV_{0}.csv".format(today.strftime('%Y%m%d'))
+else:
+    raise Exception("No need to update performance since today is not weekday.")
+dateFormat = '%m/%d/%Y'
+basePath = 'C:/Dev/LifelongInvestorWebsite/lifelonginvestorllc.github.io/'
+basePathReport = 'C:/Dev/LifelongInvestorWebsite/lifelonginvestorllc.github.io/reports/'
+numericalColumns = ["Amount", "BM1Return", "Commodities", "Equities", "Fixed Income", "NAV"]
+performanceColumns = ["Date", "Principal", "AUM", "Benchmark"]
+performanceSaveFileName = "performance.csv"
+reportCategory = {
+    "Allocation by Asset Class": "Allocation by Asset Class",
+    "Time Period Benchmark Comparison": "Time Period Benchmark Comparison",
+    "Deposits And Withdrawals": "Deposits And Withdrawals"
+}
+
+columnName = {
+    "Allocation by Asset Class": ["Date", "Commodities", "Equities", "Fixed Income", "NAV"],
+    "Time Period Benchmark Comparison": ["Date", "BM1", "BM1Return"],
+    "Deposits And Withdrawals": ["Date", "Type", "Amount"]
+}
---
+++ performance/performance.py
@@ -0,0 +1,71 @@
+from typing import List
+
+import pandas as pd
+import os
+import numpy as np
+import datetime as dt
+from pandas_datareader import data as pdr
+import config
+import yfinance as yf
+
+yf.pdr_override()
+
+
+class performance:
+    def __init__(self):
+        self.IBRawData = None
+        self.NAVData = None
+        self.benchmarkData = None
+        self.depositAndWithdrawData = None
+        self.mergedReportData = None
+        self.performanceData = None
+        self.basePath = config.basePath
+
+    def readIBReport(self):
+        self.IBRawData = pd.read_csv(config.basePathReport + config.NAVFilename, skiprows=2, header=None, skip_blank_lines=True)
+        self.NAVData = self.filterIBReport(self.IBRawData, config.reportCategory["Allocation by Asset Class"])
+        self.benchmarkData = self.filterIBReport(self.IBRawData, config.reportCategory["Time Period Benchmark Comparison"])
+        self.depositAndWithdrawData = self.filterIBReport(self.IBRawData, config.reportCategory["Deposits And Withdrawals"])
+        self.mergedReportData = self.mergeDataByColumn(self.NAVData, self.benchmarkData, self.depositAndWithdrawData)
+        self.computePerformance()
+
+    def filterIBReport(self, df: pd.DataFrame, reportCategory: str):
+        # Use first column to filter the report category (e.g., NAV, etc). Set the header and set Date as index for merging purpose.
+        tempDF = df[df.iloc[:, 0] == reportCategory].iloc[1:, :]
+        filteredDF = tempDF.rename(columns=tempDF.iloc[0]).drop(tempDF.index[0]).dropna(axis=1)[config.columnName[reportCategory]]
+        if "Date" not in filteredDF.columns:
+            raise Exception("Date has to be in the header of the dataframe")
+        filteredDF["Date"] = pd.to_datetime(filteredDF["Date"]).dt.strftime(config.dateFormat)
+        filteredDF.set_index("Date", inplace=True)
+        return filteredDF
+
+    def mergeDataByColumn(self, *dfs, join="outer", setNumericCol=True):
+        dfs = list(dfs)
+        for i in range(1, len(dfs)):
+            dfs[i] = dfs[i].merge(dfs[i - 1], how=join, left_index=True, right_index=True).fillna(0)
+        if setNumericCol:
+            result = dfs[-1][config.numericalColumns].apply(pd.to_numeric)
+        else:
+            result = dfs[-1]
+        self.mergedReportData = result
+        return self.mergedReportData
+
+    def computePerformance(self, save=True):
+        if len(self.mergedReportData) == 0:
+            raise Exception("The merged report data is not ready yet.")
+
+        df = self.mergedReportData.copy(deep=True).reset_index()
+        df['Benchmark'] = df['Amount']
+        for i in range(1, len(df)):
+            df.loc[i, 'Benchmark'] = df.loc[i - 1, 'Benchmark'] * (df.loc[i-1, 'BM1Return'] / 100 + 1) + df.loc[i, 'Amount']
+        df['Benchmark'] = df['Benchmark'] / 10000
+        df['Principal'] = df['Amount'].cumsum() / 10000
+        df['AUM'] = df['NAV'] / 10000
+        df = df[config.performanceColumns]
+        if save:
+            df.to_csv(config.basePath + config.performanceSaveFileName, index=False)
+        return df
+
+if __name__ == '__main__':
+    report = performance()
+    report.readIBReport() # read and compute performance
